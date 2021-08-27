79257444be4a70a1ae33a2f2257124ac0a51a118
Nikhil <nikhil.sengupta10@protonmail.com>
2021-08-27 18:33:16 +0100

    Reformated all text files to separate date from headline; added new python script

---
+++ convert_to_json.py
@@ -0,0 +1,13 @@
+import json
+
+# Sample file
+amazon_data = "./raw-public/amazon-com.txt"
+
+# Result dictionary
+result = {}
+
+fields = ['date', 'title', 'links', 'duration', 'planned', 'causes', 'implications', 'fixes']
+
+# with open(amazon_data) as ad:
+
+#     for line in ad:
