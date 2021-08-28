bdaeff75117118d40e5873e31c2fa8df2dbf4e7c
Nikhil <nikhil.sengupta10@protonmail.com>
2021-08-28 17:36:52 +0100

    Created json files for all data text files with python script

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,79 +1,83 @@
 import json
+import os
 import codecs
 import re
 
-# Sample file
-amazon_data = "./raw-public/amazon-com.txt"
+directory = "./raw-public"
 
-# Result dictionary
-result = {}
-case = 0
-fields = ['date', 'title', 'links', 'duration', 'planned', 'causes', 'implications', 'fixes']
+for files in os.listdir(directory):
+    file = os.path.join(directory, files)
+    if os.path.isfile(file):
 
-with codecs.open(amazon_data, 'r', encoding='utf-8', errors='ignore') as ad:
-    while True:
-        line = ad.readline()
-        if not line:
-            break
+        # Result dictionary
+        result = {}
+        case = 0
+        fields = ['date', 'title', 'links', 'duration', 'planned', 'causes', 'implications', 'fixes']
 
-        if re.search("^\d{2}\/\d{2}\/\d{4}", line):
-            case += 1
-            entry = {}
-            entry[fields[0]] = line.strip()
-            line = ad.readline()
-            entry[fields[1]] = line.strip()
-
-            links = []
-            line = ""
+        with codecs.open(file, 'r', encoding='utf-8', errors='ignore') as ad:
             while True:
                 line = ad.readline()
-                if re.search("^http", line):
-                    link = {}
-                    link["url"] = line.strip()
-                    links.append(link)
-                else:
+                if not line:
                     break
 
-            entry[fields[2]] = links
-            
-            causes = []
-            implications = []
-            fixes = []
-            planned = ""
-            duration = ""
-            while True:
-                if re.search("^r", line):
-                    cause = {}
-                    cause["cause"] = line.strip()[2:]
-                    causes.append(cause)
+                if re.search("^\d{2}\/\d{2}\/\d{4}", line):
+                    case += 1
+                    entry = {}
+                    entry[fields[0]] = line.strip()
                     line = ad.readline()
-                elif re.search("^p", line):
-                    planned = line.strip()[2:]
-                    line = ad.readline()
-                elif re.search("^i", line):
-                    implication = {}
-                    implication["implication"] = line.strip()[2:]
-                    implications.append(implication)
-                    line = ad.readline()
-                elif re.search("^f", line):
-                    fix = {}
-                    fix["fix"] = line.strip()[2:]
-                    fixes.append(fix)
-                    line = ad.readline()
-                elif re.search("^t", line):
-                    duration = line.strip()[2:]
-                    line = ad.readline()
-                else:
-                    break
+                    entry[fields[1]] = line.strip()
 
-            entry[fields[3]] = duration
-            entry[fields[4]] = planned
-            entry[fields[5]] = causes
-            entry[fields[6]] = implications
-            entry[fields[7]] = fixes
-        
-            result["Case " + str(case)] = entry
+                    links = []
+                    line = ""
+                    while True:
+                        line = ad.readline()
+                        if re.search("^http", line):
+                            link = {}
+                            link["url"] = line.strip()
+                            links.append(link)
+                        else:
+                            break
 
-out_file = open("test.json", "w")
-json.dump(result, out_file, indent = 4)
-out_file.close()
+                    entry[fields[2]] = links
+                    
+                    causes = []
+                    implications = []
+                    fixes = []
+                    planned = ""
+                    duration = ""
+                    while True:
+                        if re.search("^r", line):
+                            cause = {}
+                            cause["cause"] = line.strip()[2:]
+                            causes.append(cause)
+                            line = ad.readline()
+                        elif re.search("^p", line):
+                            planned = line.strip()[2:]
+                            line = ad.readline()
+                        elif re.search("^i", line):
+                            implication = {}
+                            implication["implication"] = line.strip()[2:]
+                            implications.append(implication)
+                            line = ad.readline()
+                        elif re.search("^f", line):
+                            fix = {}
+                            fix["fix"] = line.strip()[2:]
+                            fixes.append(fix)
+                            line = ad.readline()
+                        elif re.search("^t", line):
+                            duration = line.strip()[2:]
+                            line = ad.readline()
+                        else:
+                            break
+
+                    entry[fields[3]] = duration
+                    entry[fields[4]] = planned
+                    entry[fields[5]] = causes
+                    entry[fields[6]] = implications
+                    entry[fields[7]] = fixes
+                
+                    result["Case " + str(case)] = entry
+
+        out_file = open(file[:-4] + ".json", "w")
+        json.dump(result, out_file, indent = 4)
+        out_file.close()
